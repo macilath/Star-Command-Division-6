a210a711f1383c587e802e648ef8b0a2d0aad7d8
John Chavez <jchavez6521@gmail.com>
2013-10-07 22:46:30 -0500

    Added Project
    Added a unity project with a camera and 2 placeholder art assets

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+    private Camera camera;
+	public float moveSpeed = 0.2f;
+	
+	void Start()
+	{
+		camera = GetComponent<Camera>();
+	}
+	
+	void FixedUpdate()
+	{
+		if(camera.transform.position.x < 10 && Input.GetAxisRaw("Horizontal") > 0)
+		{
+			camera.transform.Translate(new Vector3((Input.GetAxisRaw("Horizontal") * moveSpeed), 0, 0));
+		}
+		
+		if(camera.transform.position.x > - 10 && Input.GetAxisRaw("Horizontal") < 0)
+		{
+			camera.transform.Translate(new Vector3((Input.GetAxisRaw("Horizontal") * moveSpeed), 0, 0));
+		}
+		
+		if(camera.transform.position.y < 10 && Input.GetAxisRaw("Vertical") > 0)
+		{
+			camera.transform.Translate(new Vector3(0, (Input.GetAxisRaw("Vertical") * moveSpeed), 0));
+		}
+		
+		if (camera.transform.position.y > - 10 && Input.GetAxisRaw("Vertical") < 0)
+		{
+			camera.transform.Translate(new Vector3(0, (Input.GetAxisRaw("Vertical") * moveSpeed), 0));
+		}
+	}
+}
