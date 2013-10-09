eec36c6440bdb046eb6fd6189ae80584501870ea
Stephen TenEyck <stephen.a.teneyck@gmail.com>
2013-10-09 14:25:13 -0500

    Added Player and UnitController classes to be filled in. Began working on unit selection problem.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour, UnitController {
+
+    /*
+     * For Player Unit Selection:
+     * We can check on mouse click if the rect contains / collides with the game object
+     * Then we wait on right click (orders) to assign the unit's destination, or if another left click is detected we deselect the unit
+     */
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
