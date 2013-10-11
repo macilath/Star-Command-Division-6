d5e5902dadc06066139d8aeff58e01671074059e
Stephen Ten Eyck <macilath@gmail.com>
2013-10-10 21:50:57 -0500

    Player ship is now selectable
    Still chasing down a bug where it disappears after you give it an order

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,12 +28,8 @@ public class GameManager : MonoBehaviour {
      * 
      * The other (and probably better) way is to keep a list of all selectable units (in some kind of gamemanager) and just transform the positions of all units into screenspace. 
      * There you can simply use Rect.Contains to test if a unit is inside a rectangle.
-     * (http://docs.unity3d.com/Documentation/ScriptReference/Camera.WorldToScreenPoint.html)
-     * (http://docs.unity3d.com/Documentation/ScriptReference/Rect.Contains.html)
      * (http://answers.unity3d.com/questions/287406/rts-rectangle-selection-system.html)
      * So here in GameManager we're going to keep an array (or similar structure) of GameObjects that we want to be selectable 
-     * In the player's unit controller (which extends UnitController.cs) we can check on mouse click if the rect contains / collides with the game object
-     * Then we wait on right click (orders) to assign the unit's destination, or if another left click is detected we deselect the unit
      * 
      */
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour, UnitController {
      * We can check on mouse click if the rect contains / collides with the game object
      * Then we wait on right click (orders) to assign the unit's destination, or if another left click is detected we deselect the unit
      */
+	public int shipSpeed = 10;
+	public int shipAccel = 3;
 
 	void Start () {
 		// For level 1 we are just looking for 1 ship
@@ -21,19 +23,32 @@ public class PlayerController : MonoBehaviour, UnitController {
 	
 	void Update () {	
 		Vector3 shipPosition = Camera.main.WorldToScreenPoint(playerShip.transform.position);
-		
-		if(Input.GetMouseButtonDown(0) && isSelected == false) {
-			print("Mouse0 detected");
-			print(Input.mousePosition);
-			// Create our bounding rectangle
-			Rect boundingRect = new Rect(Input.mousePosition.x, Input.mousePosition.y, 10, 10);
-			// See if our Ship object is in the bounding rectangle
-			if(boundingRect.Contains(shipPosition)){
-				print("Found object" + manager.Selectable[0]);
-				isSelected = true;
-			}
-		}
-		else isSelected = false;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (isSelected == false) {
+                print("Mouse0 detected");
+                print(Input.mousePosition);
+                // Create our bounding rectangle - the size of which still needs some testing/debugging
+                Rect boundingRect = new Rect(Input.mousePosition.x - 75, Input.mousePosition.y - 75, 150, 150);
+                // See if our Ship object is in the bounding rectangle
+                if (boundingRect.Contains(shipPosition)) {
+                    print("Found object");
+                    isSelected = true;
+                }
+            }
+            else isSelected = false; // Clear out previous selection
+        }
+
+        // Assign movement orders to ship
+        if (Input.GetMouseButtonDown(1) && isSelected == true) {
+            print("Orders: GOTO " + Input.mousePosition);
+			// Later we need to detect if there is an enemy at the coordinates we just chose
+			// If so, follow that enemy and attack (since enemies will continue to move)
+			Vector3 destination = Input.mousePosition; 
+			//transform.position = Vector3.Lerp(shipPosition, destination, 0.2f);
+			destination.z = 0; 
+			transform.position = destination;
+        }
 	}
 	
 }
