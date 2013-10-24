24979204ff627c83a32f6e0d8168a495d703ef84
John Chavez <jchavez6521@gmail.com>
2013-10-23 20:59:09 -0500

    Added Explosions
    The ships explode when they hit an asteroid

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+	
+	public GameObject Explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Ship")
+		{
+			Instantiate(Explosion, other.transform.position, Quaternion.identity);
+			Destroy(other.gameObject);
+		}
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	
+	public float Timer;
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - startTime > Timer){
+			Destroy(this.gameObject);	
+		}
+	}
+}
