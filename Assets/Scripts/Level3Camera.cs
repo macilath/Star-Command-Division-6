89bcb4d0c1b30e79eb2bb1bddfb68b4fc397ba56
qantravon <qantravon@gmail.com>
2013-11-20 20:33:49 -0600

    added camera follow player for level 3

---
+++ Assets/Scripts/Level3Camera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3Camera : MonoBehaviour {
+
+    public Camera camera;
+	public GameObject player;
+	
+	public void Awake()
+    {
+		camera = GetComponent<Camera>();
+        player = GameObject.Find("PlayerAnchor");
+    }
+	
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+	}
+
+	void FixedUpdate()
+	{
+		snapToPlayer();
+	}
+	
+	public void snapToPlayer()
+	{
+        Vector3 playerPos = player.transform.position;
+		playerPos.y = camera.transform.position.y;
+			
+		Vector3 amountMoved = shipPos - camera.transform.position;
+			
+		camera.transform.position = playerPos;
+	}
+}
